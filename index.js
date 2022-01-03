df22e478cd961cdd62b647120219532c38d4a37c
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-01-03 19:46:45 -0300

    revise main

--- index.js
+++ index.js
@@ -38,3 +38,8 @@ const botonVerMas = () => {
     //    mostrarTrago(idTrago)
 }}}
 
+fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
+.then((res) =>  res.json())
+.then((data) => {
+    console.log(data.drinks)
+})
